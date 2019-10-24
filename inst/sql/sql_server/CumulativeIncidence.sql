5a4481f80037a49a8637594fcb8369ae6804aa0d
cukarthik <cukarthik@users.noreply.github.com>
2019-10-23 23:01:42 -0400

    Added database parameter for observation_period

--- inst/sql/sql_server/CumulativeIncidence.sql
+++ inst/sql/sql_server/CumulativeIncidence.sql
@@ -12,7 +12,7 @@ SELECT cohort1.subject_id,
 		ELSE CAST(0 AS INT)
 	END AS has_outcome
 FROM @target_database_schema.@study_cohort_table cohort1
-INNER JOIN observation_period
+INNER JOIN @cdm_database_schema.observation_period
 	ON cohort1.subject_id = observation_period.person_id
 	AND cohort1.cohort_start_date >= observation_period_start_date
 	AND cohort1.cohort_start_date <= observation_period_end_date
